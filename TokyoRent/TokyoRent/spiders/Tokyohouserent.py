1cfff1db7cbb928306c06089ab7d1f156f8f0923
Nouri-Muhammad <nouri.muhammad91@gmail.com>
2024-01-21 10:54:02 +0330

    Complete the code to script website for the first n pages

--- TokyoRent/TokyoRent/spiders/Tokyohouserent.py
+++ TokyoRent/TokyoRent/spiders/Tokyohouserent.py
@@ -7,23 +7,41 @@ class TokyohouserentSpider(scrapy.Spider):
     name = "Tokyohouserent"
     allowed_domains = ["apartments.gaijinpot.com"]
     start_urls = ["https://apartments.gaijinpot.com"]
-    count = 0
+    while True:
+        try:
+            count = int(input("how many pages do you want to scrape?: "))
+            break
+        except ValueError:
+             print("Invalid Entry")
 
     def parse(self, response):
         apartments = response.xpath("//div[contains(@class,'property-listing')]")
-
         for apartment in apartments:
-            yield{
-                'detail': apartment.xpath(".//div[@class='listing-item listing-title']//span[@itemprop='address']/text()").getall(),
-                'price': apartment.xpath("./div[@class='listing-body']/div[@class='listing-right-col']/div[@class='listing-item'][1]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
-                'size': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][1]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
-                'deposite': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][2]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
-                'key money': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][3]/span[@class='']/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
-                'floor': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][4]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
-                'year built': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][5]/text()[normalize-space()]").get(),
-                'nearest station': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][6]/span[@itemprop='name']/text()").get(),
-            }
-
+                text = apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][4]/span/text()[normalize-space()]").get()
+                if 'Floor' == text:
+                        yield{
+                            'detail': apartment.xpath(".//div[@class='listing-item listing-title']//span[@itemprop='address']/text()").getall(),
+                            'price': apartment.xpath("./div[@class='listing-body']/div[@class='listing-right-col']/div[@class='listing-item'][1]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                            'size': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][1]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                            'deposite': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][2]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                            'key money': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][3]/span[@class='']/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                            'floor': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][4]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                            'year built': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][5]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                            'nearest station': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][6]/span[@itemprop='name']/text()").get(),
+                        }
+                else:
+                    floor = None
+                    yield{
+                        'detail': apartment.xpath(".//div[@class='listing-item listing-title']//span[@itemprop='address']/text()").getall(),
+                        'price': apartment.xpath("./div[@class='listing-body']/div[@class='listing-right-col']/div[@class='listing-item'][1]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                        'size': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][1]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                        'deposite': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][2]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                        'key money': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][3]/span[@class='']/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                        'floor': floor,
+                        'year built': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][4]/text()[normalize-space()]").get().replace('\n', '').replace('\t', ''),
+                        'nearest station': apartment.xpath(".//div[@class='listing-info']/div[@class='listing-right-col']/div[@class='listing-item'][5]/span[@itemprop='name']/text()").get(),
+                    }
+        
         next_page = response.xpath(".//li[@class='pagination-next']/a/@href").get()
         if next_page is not None and self.count<400:
             self.count += 1
